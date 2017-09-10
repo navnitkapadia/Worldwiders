54bbaa4786592fd29f46c7a2462a6aee29ec9b4f
navnitkapadia <navnitkapadia@gmail.com>
2017-09-10 17:24:31 +0530

    code refectored

--- js/messages.js
+++ js/messages.js
@@ -1,21 +1,17 @@
 function resize()
 {
-
     var heights = window.innerHeight - 150;
     var el = document.getElementsByClassName('chat-message');
     for(var i = 0; i< el.length; i++){
-        
         el[i].style.height = heights + "px";
         el[i].scrollTop = el[i].scrollHeight;
-        }
-         
+        }        
 }
 window.onresize = function() {
     resize();
 };
 
 $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-   
     var target = $(e.target).attr("href") // activated tab
     var conid = $(e.target).attr("con-id") // activated tab'
     var killId = setTimeout(function() {
@@ -25,7 +21,6 @@ $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
       var getdata = setInterval(function() {
         $(".open-"+conid).load("api/get_messages.php?c_id="+conid);
    }, 2000);
-   
   });
 
 function getMessages(btnid,convid,sender,receiver){
